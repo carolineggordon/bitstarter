dce9af205612119a86a9eaeb2404fda82e030562
carolineggordon <carolinegordon531@gmail.com>
2013-07-21 02:35:04 +0000

    require added

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
    response.send(fs.readFileSync('index.html','utf8'));
    // response.send('Hello World2!');
